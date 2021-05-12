5599650258820168a7b3e93855d59a4bf26a671d
alisq <ali.shamas.qadeer@gmail.com>
2021-05-12 13:39:58 -0400

    added student click.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ var url="http://2021.ocadu.gd/feed/json/";
 fetch(url)
 .then(response => response.json())
   .then(p => {
-    console.log(p.length)
+    //console.log(p.length)
       for (i=0;i<p.length;i++) {
-        console.log(p[i])
-        $("#students").append("<div class='student'><div class='initials'>"+intialize(p[i].field_last_name)+"</div><div class='profile-img' style='background-image:url(http://2021.ocadu.gd"+p[i].field_profile_image+")'></div><div class='thumb-img' style='background-image:url(http://2021.ocadu.gd"+p[i].field_thumbnail_image+")'></div><div class='profile-name'>"+p[i].field_last_name+"</div></div>")
+        //console.log(p[i])
+        $("#students").append("<div class='student' id='student-"+p[i].nid+"'><div class='initials'>"+intialize(p[i].field_last_name)+"</div><div class='profile-img' style='background-image:url(http://2021.ocadu.gd"+p[i].field_profile_image+")'></div><div class='thumb-img' style='background-image:url(http://2021.ocadu.gd"+p[i].field_thumbnail_image+")'></div><div class='profile-name'>"+p[i].field_last_name+"</div></div>")
 
-        
+         
       }
     
   });
@@ -17,14 +17,29 @@ fetch(url)
   
      
 
-//   <div class='student'>
+//   <div class='student'  id='student-"+p[i].nid+"'>
 //   <div class='initials'>"+intialize(p[i].field_last_name)+"</div>
 //   <div class='profile-img' style='background-image:url(http://2021.ocadu.gd"+p[i].field_profile_image+")'></div>
 //   <div class='thumb-img' style="background-image:url(http://2021.ocadu.gd"+p[i].field_thumbnail_image+")'></div>
 //    <div class='profile-name'>"+p[i].field_last_name+"</div>
 // </div>
 
+$(document).on("click",".student", function(){
+  nid = $(this).attr("id").replace("student-","");
 
+  
+  fetch("http://2021.ocadu.gd/s/"+nid)
+.then(response => response.json())
+  .then(p => {
+      console.log(p);
+    $("#students, #site-title").fadeOut(200).delay(200).remove();
+      
+      $("<div></div>")
+          .append("<h2>"+p[0].title+"</h2>")
+          .appendTo("#student")
+    
+  });
+})
 
 
   //function to convert to initials
